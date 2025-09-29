572b4a3d1a806be3db76da478e0a0dfe6c7159bd
mortiz308 <163943566+mortiz308@users.noreply.github.com>
2025-09-28 21:21:02 -0500

    3.4 Assignment 09/28/25 21:21
    Functions, parameters, events, methods

---
+++ app.js
@@ -0,0 +1,10 @@
+const company = "MO Digital Media";
+const name = "Marco";
+
+function toUpper(text) {
+    const upperCased = text.toUpperCase();
+    console.log(upperCased);
+    }
+
+toUpper(company);
+toUpper(name)
